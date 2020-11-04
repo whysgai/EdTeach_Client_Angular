80bba19f3bbdf255ae5ad2411639e790f80ee5dd
Will Cohen <will.r.cohen@gmail.com>
2020-11-04 16:55:27 -0500

    A7 feat: Implement basic dynamic data rendering

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseListComponent implements OnInit {
 
+  title = 'Course List Component';
+  courses = [
+    {_id: '123', title: 'Course A'},
+    {_id: '345', title: 'Course B'},
+    {_id: '567', title: 'Course C'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
