7274aa896239d18255073350b07cd8f10e54aceb
Will Cohen <will.r.cohen@gmail.com>
2020-11-08 10:25:37 -0500

    A7 feat: Implement basic course CRUD service

---
+++ src/services/CourseService.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class CourseService {
+
+  createCourse = () =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/cohenw/courses`, {
+      method: 'POST',
+      body: JSON.stringify({title: 'New Course'}),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+
+
+  findAllCourses = () =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/cohenw/courses`)
+      .then(response => response.json())
+
+
+  updateCourse = (course) =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/cohenw/courses/${course._id}`, {
+      method: 'PUT',
+      body: JSON.stringify(course),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+
+
+  deleteCourse = (id) =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/cohenw/courses/${id}`, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+
+
+
+
+}
