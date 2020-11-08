ec5636a96c99e2b8380070c51a39172596a7c9de
Will Cohen <will.r.cohen@gmail.com>
2020-11-08 10:53:01 -0500

    A7 feat: Add services to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './course-list/course-list.component';
 // import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import {CourseService} from '../services/CourseService';
+import {ModuleServices} from '../services/ModuleServices';
+import {LessonServices} from '../services/LessonServices';
+import {TopicServices} from '../services/TopicServices';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,12 @@ import { FormsModule } from '@angular/forms';
     // AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    CourseService,
+    ModuleServices,
+    LessonServices,
+    TopicServices
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
