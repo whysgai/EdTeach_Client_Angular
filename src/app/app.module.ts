54346779f4d1b97de385cb0dd2a7f3aa64a56cfb
Will Cohen <will.r.cohen@gmail.com>
2020-11-10 21:54:41 -0500

    A7 feat: statically render lists in separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,21 @@ import {CourseService} from '../services/CourseService';
 import {ModuleServices} from '../services/ModuleServices';
 import {LessonServices} from '../services/LessonServices';
 import {TopicServices} from '../services/TopicServices';
+import { CourseTableComponentComponent } from './course-table-component/course-table-component.component';
+import { CourseViewerComponentComponent } from './course-viewer-component/course-viewer-component.component';
+import { ModuleListComponentComponent } from './module-list-component/module-list-component.component';
+import { LessonTabsComponentComponent } from './lesson-tabs-component/lesson-tabs-component.component';
+import { TopicPillsComponentComponent } from './topic-pills-component/topic-pills-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    CourseTableComponentComponent,
+    CourseViewerComponentComponent,
+    ModuleListComponentComponent,
+    LessonTabsComponentComponent,
+    TopicPillsComponentComponent
   ],
   imports: [
     BrowserModule,
