27e40d8ffd5d6e81a574b37e6e0f050acf95f029
Will Cohen <will.r.cohen@gmail.com>
2021-02-05 15:19:28 -0500

    chore: Update angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 @NgModule({
   // declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
   ],
   exports: [
     RouterModule
