6ca3f36b175250d61886efa367a9c2f708af4aa4
Will Cohen <will.r.cohen@gmail.com>
2021-02-05 15:44:56 -0500

    fix: Added correct app name to server.js file

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/app'));
+app.use(express.static('./dist/cs5610-fa20-cohenw-client-angular'));
 
 app.get('/*', function(req,res) {    
-    res.sendFile(path.join(__dirname+'/dist/app/index.html'));
+    res.sendFile(path.join('index.html', {root: 'dist/cs5610-fa20-cohenw-client-angular'}));
 });
 
 // Start the app by listening on the default Heroku port
